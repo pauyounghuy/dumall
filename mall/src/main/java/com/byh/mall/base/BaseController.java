2a27b3361cf5ee09827a761bf6385d6e743986a7
byh <865967315@qq.com>
2020-05-17 01:22:07 +0800

    1

--- mall/src/main/java/com/byh/mall/base/BaseController.java
+++ mall/src/main/java/com/byh/mall/base/BaseController.java
@@ -1,9 +1,6 @@
 package com.byh.mall.base;
 import com.byh.mall.utils.ConfigurationUtils;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.PropertySource;
-import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Component;
 
 import java.io.File;
